23e20c0824c0f711eb04791f953dfae3536e4574
pistapixie <hleehslee@gmail.com>
2024-04-07 06:06:55 +0900

    [Feat]axios 세팅

---
+++ src/layout/Applayout.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Outlet } from "react-router-dom";
+// import NavDropdown from "react-bootstrap/NavDropdown";
+
+const Applayout = () => {
+  return (
+    <div>
+      <Navbar expand="lg" style={{ backgroundColor: "#000000" }}>
+        <Container fluid>
+          <Navbar.Brand href="#">
+            {" "}
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmueGL6uhhXahumOw7vxpIGEQD2w_FKfcKI3c9s-BxMgBYtgvDXRcO2G4WPapJxvnIraU&usqp=CAU"
+              alt="/"
+              style={{ maxWidth: "100px", height: "auto" }}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="/" style={{ color: "white" }}>
+                Home
+              </Nav.Link>
+              <Nav.Link href="/movies" style={{ color: "white" }}>
+                Movies
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-danger">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Applayout;
