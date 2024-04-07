1a8059aac2b682b6cd0c9f07f1d105ec812b0a3c
pistapixie <hleehslee@gmail.com>
2024-04-08 08:06:34 +0900

    [Feat]banner 마크업, 스타일링

--- src/App.js
+++ src/App.js
@@ -14,16 +14,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //flqb  /movies/:id/reviews
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Applayout />}>
-        <Route index element={<Homepage />} />
-        <Route path="movies">
-          <Route index element={<MoviePage />} />
-          <Route path=":id" element={<MovieDetailPage />} />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Applayout />}>
+          <Route index element={<Homepage />} />
+          <Route path="movies">
+            <Route index element={<MoviePage />} />
+            <Route path=":id" element={<MovieDetailPage />} />
+          </Route>
         </Route>
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
   );
 }
 
